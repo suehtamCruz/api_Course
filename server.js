babf024675a6096be36576ff11683c4bb88c7c16
Matheus Cruz <matheuscz3110@gmail.com>
2020-09-06 11:55:50 -0300

    up server

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const http = require('http');
 
-const server = http.createServer(function (require,response) {
-    response.end(`
-        <html>
-            <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width inicial-scale=1.0">
-                <title> Testando forma de node </title>
-            </head>
-        
-            <body>
-                <h2>
-                    Matheus é lindao
-                </h2>
-            </body>
-        
-        </html>
-    `);
+const server = http.createServer(function (require, response) {
+
+    if(require.url == '/oi'){
+        response.end(`
+            <html>
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width inicial-scale=1.0">
+                    <title> Testando forma de node </title>
+                </head>
+            
+                <body>
+                    <h2>
+                        Oi pra voce tambem meu brother 
+                    </h2>
+                </body>
+            
+            </html>
+        `);
+    }
+    if (require.url == '/') {
+        response.end(`
+            <html>
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width inicial-scale=1.0">
+                    <title> Testando forma de node </title>
+                </head>
+            
+                <body>
+                    <h2>
+                       Index
+                    </h2>
+                </body>
+            
+            </html>
+        `);
+    }
 });
+
 server.listen(3000);
